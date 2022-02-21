cff051f0b1c150738586a56b64fbca86a7c637bd
NoCereaL <therealskm@hotmail.com>
2022-02-21 23:16:23 +0000

    Backend Update
    Completed Login + Register

--- Desk Booking Draft/Assets/Scripts/Back-End/SQLHandler.cs
+++ Desk Booking Draft/Assets/Scripts/Back-End/SQLHandler.cs
@@ -2,42 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using System.Linq;
-using System.Text;
-using System.Collections.Specialized;
-using System.Net;
-using System.IO;
-using System;
+using UnityEngine.SceneManagement;
+
 
 public class SQLHandler : MonoBehaviour
 {
-    public string loginURL = "http://localhost/unity_test/login.php";
-    public string usernameURL = "http://localhost/unity_test/username.php";
-    public string sendURL = "http://localhost/unity_test/send.php";
-    public Text statusText;
-    public Text usernameText;
+    //Localhost Server URL's
+    private string localLoginURL = "http://localhost/unity_test/login.php";
+    private string localRegisterURL = "http://localhost/unity_test/register.php";
+
+    //Dedicated Server URL's
+    private string loginURL = "https://moyanask.com/EPTeam1/login.php";
+    private string registerURL = "https://moyanask.com/EPTeam1/register.php";
+    [SerializeField] GameObject incorrectText;
 
     public InputField usernameField;
     public InputField passwordField;
     // Start is called before the first frame update
     void Start()
     {
-        //StartCoroutine(GetUsername());
-    }
 
-    IEnumerator GetUsername()
-    {
-        usernameText.text = "Loading Scores";
-        WWW hs_get = new WWW(usernameURL);
-        yield return hs_get;
-        if (hs_get.error != null)
-        {
-            print("There was an error getting the high score: " + hs_get.error);
-        }
-        else
-        {
-            usernameText.text = hs_get.text; // this is a GUIText that will display the scores in game.
-        }
     }
 
     public void CallRegisterUser()
@@ -45,21 +29,45 @@ public class SQLHandler : MonoBehaviour
         StartCoroutine(RegisterUser());
     }
 
+    public void CallLoginUser()
+    {
+        StartCoroutine(LoginUser());
+    }
+
     IEnumerator RegisterUser()
     {
-        WWWForm form = new WWWForm();
-        form.AddField("user", usernameField.text);
+        WWWForm form = new WWWForm();                   //Create a form
+        form.AddField("user", usernameField.text);          //Using the post method make sure 1st parameter same in php
         form.AddField("pass", passwordField.text);
-        WWW www = new WWW("http://localhost/unity_test/send.php", form);
+        WWW www = new WWW(registerURL, form);           //We Send the web request form to the php link
         yield return www;
-        if(www.text == "Approved")
+        if(www.text.Contains("Approved"))                      //Checks weather register.php script ran successfully
         {
             Debug.Log("User Created Successfully");
         }
         else
         {
-            Debug.Log("Unsuccessfull: " + www.text);
+            Debug.Log("Unsuccessfull Result: " + www.text);        //print out php/sql error
         }
     }
 
+    IEnumerator LoginUser()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("user", usernameField.text);
+        form.AddField("pass", passwordField.text);
+        WWW www = new WWW(loginURL, form);
+        yield return www;
+        if (www.text.Contains("Approved"))      //Checks weather login.php script ran successfully
+        {
+            Debug.Log("Login Successfully");
+            incorrectText.SetActive(false);
+            SceneManager.LoadScene(1);          //Signs you into the system by loading the index scene
+        }
+        else
+        {
+            Debug.Log("Unsuccessfull Result: " + www.text);
+            incorrectText.SetActive(true);
+        }
+    }
 }
--- Desk Booking Draft/Assets/Scripts/Front-End/ButtonActionsScript.cs
+++ Desk Booking Draft/Assets/Scripts/Front-End/ButtonActionsScript.cs
@@ -6,18 +6,6 @@ using UnityEngine.UI;
 
 public class ButtonActionsScript : MonoBehaviour
 {
-    [SerializeField] InputField username;
-    [SerializeField] InputField password;
-    [SerializeField] GameObject incorrectText;
-    public void Login()
-    {
-        if (username.text == "Simon" && password.text == "Simon")
-        {
-            SceneManager.LoadScene(1);
-        }
-        else
-        {
-            incorrectText.SetActive(true);
-        }
-    }
+
+ 
 }
